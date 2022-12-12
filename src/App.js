d5721fc3048893e1a963f2879a5e7c1dd35287b6
Abhishek Purohit <37204429+DeveloperGitHub89@users.noreply.github.com>
2022-12-12 16:36:21 +0530

    router added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AllNews } from './components/AllNews';
+import { Home } from './components/Home';
+import { NavigationBar } from './components/NavigationBar';
 import { TopHeadlines } from './components/TopHeadlines';
 
 function App() {
   return (
-    <div>
-      <AllNews></AllNews>
-      {/* <TopHeadlines></TopHeadlines> */}
-    </div>
+    <BrowserRouter>
+      <div>
+        <NavigationBar></NavigationBar>
+        <Routes>
+          <Route path='/' element={<Home></Home>}></Route>
+          <Route path='/all-news' element={<AllNews></AllNews>}></Route>
+          <Route path='/top-headlines' element={<TopHeadlines></TopHeadlines>}></Route>
+        </Routes>
+
+      </div>
+    </BrowserRouter>
+
   );
 }
 
